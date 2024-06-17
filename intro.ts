964b30fd5aab2635e3d5232c60308bc228a99e3d
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-06-17 23:56:23 +0530

    typescript

---
+++ intro.ts
@@ -0,0 +1,11 @@
+
+let user = {name :"ankur", age:23}
+
+console.log("ankur");
+console.log(user.name);
+
+//number  // string
+// boolean // null 
+// undefined // void
+// object // Array // Tuples  // Any
+// Never // Unknown 
---
+++ variableMe.ts
@@ -0,0 +1,21 @@
+let grretings : string = "Hello GHItesh"
+
+console.log(grretings);
+
+let userId: number = 78888
+userId.toFixed()
+
+// boolean
+
+let isLoggedIn : boolean = false
+
+let hero;
+
+function getHero() {
+    return "thor"
+}
+
+hero = getHero();
+
+export {}
+
